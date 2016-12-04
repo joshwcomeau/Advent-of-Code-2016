5cfb254ca1cf6eba36607c40f6de1ae87ad04621
Joshua Comeau <joshwcomeau@gmail.com>
2016-12-03 20:37:58 -0500

    Working solution for the wrong question :(

--- solutions/02/02.test.js
+++ solutions/02/02.test.js
@@ -2,16 +2,133 @@ const { expect } = require('chai');
 
 const {
   sumArrays,
+  clamp,
   getCoordinateShiftForDirection,
+  outOfBounds,
   moveWithinMatrix,
 } = require('./index.js');
 
 
 describe.only('Problem 02', () => {
+  const matrix = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+  ];
+
   describe('sumArrays', () => {
     it('sums values in the same positions', () => {
       const actualResult = sumArrays([1, 2], [3, 4]);
       const expectedResult = [4, 6];
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+  });
+
+  describe('clamp', () => {
+    it('clamps the minimum', () => {
+      const actualResult = clamp({ min: 0, max: 2 })(-1);
+      const expectedResult = 0;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('clamps the maximum', () => {
+      const actualResult = clamp({ min: 0, max: 2 })(3);
+      const expectedResult = 2;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+  });
+
+  describe('getCoordinateShiftForDirection', () => {
+    it('handles UP', () => {
+      const actualResult = getCoordinateShiftForDirection('U');
+      const expectedResult = [-1, 0];
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+    it('handles LEFT', () => {
+      const actualResult = getCoordinateShiftForDirection('L');
+      const expectedResult = [0, -1];
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+    it('handles RIGHT', () => {
+      const actualResult = getCoordinateShiftForDirection('R');
+      const expectedResult = [0, 1];
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+    it('handles DOWN', () => {
+      const actualResult = getCoordinateShiftForDirection('D');
+      const expectedResult = [1, 0];
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+  });
+
+  describe('outOfBounds', () => {
+    it('returns true if the X coordinate is too low', () => {
+      const actualResult = outOfBounds(matrix, [-1, 1]);
+      const expectedResult = true;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('returns true if the X coordinate is too high', () => {
+      const actualResult = outOfBounds(matrix, [3, 1]);
+      const expectedResult = true;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('returns true if the Y coordinate is too low', () => {
+      const actualResult = outOfBounds(matrix, [1, -2]);
+      const expectedResult = true;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('returns true if the Y coordinate is too high', () => {
+      const actualResult = outOfBounds(matrix, [1, 5]);
+      const expectedResult = true;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('returns false when both values are within the matrix', () => {
+      const actualResult = outOfBounds(matrix, [1, 1]);
+      const expectedResult = false;
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+  })
+
+  describe('moveWithinMatrix', () => {
+    it('handles a simple instruction', () => {
+      const instructions = 'UL';
+
+      const actualResult = moveWithinMatrix({ matrix, instructions });
+      const expectedResult = '21';
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('handles a slightly more complex instruction', () => {
+      const instructions = 'ULDRRDLU';
+
+      const actualResult = moveWithinMatrix({ matrix, instructions });
+      const expectedResult = '21456985';
+
+      expect(actualResult).to.deep.equal(expectedResult);
+    });
+
+    it('clamps within the matrix', () => {
+      const instructions = 'UUUUUL';
+
+      const actualResult = moveWithinMatrix({ matrix, instructions });
+      const expectedResult = '21';
 
       expect(actualResult).to.deep.equal(expectedResult);
     });
--- solutions/02/index.js
+++ solutions/02/index.js
@@ -4,29 +4,96 @@ const sumArrays = (arr1, arr2) => (
   arr1.map((element, index) => element + arr2[index])
 );
 
-function getCoordinateShiftForDirection(coords, direction) {
- switch (direction) {
-   case 'U': return [-1, 0];
-   case 'L': return [0, -1];
-   case 'R': return [0, 1];
-   case 'D': return [1, 0];
-   default: throw new Error("Invalid direction provided");
- }
+const clamp = ({ min, max }) => num => (
+  Math.max(min, Math.min(num, max))
+);
+
+const outOfBounds = (matrix, [x, y]) => {
+  // For the purposes of this problem, we'll assume that the matrix is always
+  // a square (equal length X and Y axes).
+  const min = 0;
+  const max = matrix.length - 1
+
+  return (
+    x < min || x > max ||
+    y < min || y > max
+  );
+}
+
+const getCoordinateShiftForDirection = direction => {
+  switch (direction) {
+    case 'U': return [-1, 0];
+    case 'L': return [0, -1];
+    case 'R': return [0, 1];
+    case 'D': return [1, 0];
+    default: throw new Error("Invalid direction provided");
+  }
+}
+
+function moveWithinMatrix({
+  matrix,
+  code = '',
+  currentPosition = [1, 1],
+  instructions,
+}) {
+  const clamper = clamp({ min: 0, max: matrix.length - 1 });
+
+  const direction = instructions[0];
+  const offset = getCoordinateShiftForDirection(direction);
+
+  const newPosition = sumArrays(currentPosition, offset);
+
+  const isOutOfBounds = outOfBounds(matrix, newPosition)
+
+  // Ignore any 'illegal' moves that fall outside the given matrix
+  if (!isOutOfBounds) {
+    const [x, y] = newPosition;
+    const digit = matrix[x][y];
+
+    code += digit;
+  }
+
+  // If that's the last of the instructions, we're done!
+  const nextInstructions = instructions.substr(1);
+
+  if (!nextInstructions) {
+    return code;
+  }
+
+  return moveWithinMatrix({
+    matrix,
+    code,
+    currentPosition: isOutOfBounds ? currentPosition : newPosition,
+    instructions: nextInstructions,
+  });
 }
 
 
+const solve = (part) => {
+  const input = require('./data');
+  const matrix = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+  ]
 
-
-function moveWithinMatrix(matrix, currentPosition, direction) {
- const offset = getCoordinateShiftForDirection(currentPosition, direction);
-
- // TODO: Ensure that resulting value is positive / clamped to 0
-
- return sumArrays(currentPosition, offset);
+  return moveWithinMatrix({ matrix, instructions: input })
 }
+
+
+// Run the solution(s), if any, requested by the command line arguments.
+const [node, filename, ...solutions] = process.argv;
+
+solutions.forEach(solution => {
+  console.log(`Solving for ${solution}:`, solve(solution));
+});
+
+
 
 module.exports = {
   sumArrays,
+  clamp,
   getCoordinateShiftForDirection,
+  outOfBounds,
   moveWithinMatrix,
 };
